aaa0e931e2acdbd4ababffbd36f8197ddd628052
Zaki <menza.zm@gmail.com>
2023-04-17 10:29:12 +0600

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Zaki on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
